6f593816f34fc19b065fb5aa1a47135d2b7e9c4b
chamikalaks <chamikalaks@gmail.com>
2023-03-05 16:02:11 +0000

    average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -7,7 +7,7 @@ public class Average {
      * Part of the challenge is that the method recieves an array of ints, which do not produce decimal values
      * even in operations where they should! (eg: 3/2=1 if 3 is of type int.)
      *
-     * So, before you divide the sum of all numbers by the amount of numbers in order to produce the average,
+     * So, before you divide the sum of all numbers by  amount of numbers in order to produce the average,
      * you'll need do convert the sum to type double. How can we do that?
      *
      * One of the best resources for answering technical questions is StackOverflow. You should familiarize yourself
